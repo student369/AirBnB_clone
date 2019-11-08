bc2aa40db8af1f7d55ea42f37a061d59adc3f97d
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-08 10:02:51 -0500

    Adding the tests/test_models/test_base_model.py file.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Unittest for Base class"""
-import models.basemodel as b
-import io
-import pep8
+"""Unittest for BaseModel class"""
+import models.base_model as b
+import pip8
 import unittest
-from models.basemodel import BaseModel
+from models.base_model import BaseModel
 
 
 class TestBase(unittest.TestCase):
     """Base class tests"""
+
+    def setUp(self):
+        """Base classes to the tests"""
+        self.o0 = BaseModel()
 
     def test_pep8(self):
         """test the pep8 in the files"""
@@ -46,112 +49,6 @@ class TestBase(unittest.TestCase):
     def test_init_doc(self):
         """A test of constructor doc"""
         self.assertTrue(len(BaseModel.__init__.__doc__) > 10)
-
-    def test_json_string_doc(self):
-        """A test of to_json_string function doc"""
-        self.assertTrue(
-            len(Base.to_json_string.__doc__) > 10
-        )
-
-    def test_save_to_file_doc(self):
-        """A test of save_to_file function doc"""
-        self.assertTrue(
-            len(Base.save_to_file.__doc__) > 10
-        )
-
-    def test_from_json_string_doc(self):
-        """A test of from_json_string function doc"""
-        self.assertTrue(
-            len(Base.from_json_string.__doc__) > 10
-        )
-
-    def test_base_id0(self):
-        """Test the correct id of instance"""
-        o = Base()
-        self.assertEqual(o.id, 1)
-
-    def test_base_id1(self):
-        """Test the correct id of instance"""
-        o = Base()
-        self.assertEqual(o.id, 2)
-
-    def test_base_id2(self):
-        """Test the correct id of instance"""
-        o = Base()
-        self.assertEqual(o.id, 3)
-
-    def test_base_id3(self):
-        """Test the correct id of instance"""
-        o = Base(12)
-        self.assertEqual(o.id, 12)
-
-    def test_base_id4(self):
-        """Test the correct id of instance"""
-        o = Base()
-        self.assertEqual(o.id, 4)
-
-    def test_base_id5(self):
-        """Test the correct id of instance"""
-        o = Base(89)
-        self.assertEqual(o.id, 89)
-
-    def test_json_string0(self):
-        """Test if exist the to_json_strin function"""
-        self.assertTrue(Base.to_json_string(None))
-
-    def test_json_string1(self):
-        """Test if exist the to_json_string function"""
-        self.assertTrue(Base.to_json_string([]))
-
-    def test_json_string2(self):
-        """Test if exist the to_json_string function"""
-        self.assertTrue(
-            Base.to_json_string([{'id': 12}])
-        )
-
-    def test_json_string3(self):
-        """Test if the to_json_string function
-        retuns an string
-        """
-        self.assertIsInstance(
-            Base.to_json_string([{'id': 12}]),
-            str
-        )
-
-    def test_json_from_json0(self):
-        """Test if the from_json_strin function
-        retuns a empty list
-        """
-        self.assertEqual(
-            Base.from_json_string(None),
-            []
-        )
-
-    def test_json_from_json1(self):
-        """Test if the from_json_strin function
-        retuns a empty list
-        """
-        self.assertEqual(
-            Base.from_json_string("[]"),
-            []
-        )
-
-    def test_json_from_json2(self):
-        """Test if the from_json_strin exist
-        with an specific parameter
-        """
-        self.assertTrue(
-            Base.from_json_string('[{"id": 89}]')
-        )
-
-    def test_json_from_json3(self):
-        """Test if the from_json_strin exist
-        with an specific parameter
-        """
-        self.assertIsInstance(
-            Base.from_json_string('[{"id": 89}]'),
-            list
-        )
 
 
 if __name__ == "__main__":
