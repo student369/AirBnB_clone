88e1f89e1f30720b7caffe2c866687d4f991a9ba
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-08 13:21:27 -0500

    Adding the amenity.py file.

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module amenity
+
+This module contains the Amenity class
+
+"""
+from models import base_model as b
+
+
+class Amenity(b.BaseModel):
+    """Amenity class
+
+    A simple Amenity class
+    """
+    name = ""
