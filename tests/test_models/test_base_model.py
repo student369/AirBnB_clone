db3f7205164909abe2903cbda535493ed3e5e947
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-07 22:01:10 -0500

    mod base model.

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python3
+"""Unittest for Base class"""
+import models.basemodel as b
+import io
+import pep8
+import unittest
+from models.basemodel import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """Base class tests"""
+
+    def test_pep8(self):
+        """test the pep8 in the files"""
+        p8 = pep8.StyleGuide(quiet=True)
+        ret = p8.check_files(
+            ['models/base_model.py',
+             'models/engine/file_storage.py',
+             'console.py',
+             'models/user.py',
+             'models/state.py',
+             'models/city.py',
+             'models/amenity.py',
+             'models/place.py',
+             'models/review.py',
+             'test_base.py',
+             'test_user.py',
+             'test_state.py',
+             'test_city.py',
+             'test_amenity.py',
+             'test_place.py',
+             'test_review.py']
+        )
+        self.assertEqual(
+            ret.total_errors, 0,
+            "Pep 8 errors")
+
+    def test_module_doc(self):
+        """A test of module doc"""
+        self.assertTrue(len(b.__doc__) > 10)
+
+    def test_class_doc(self):
+        """A test of class doc"""
+        self.assertTrue(len(BaseModel.__doc__) > 10)
+
+    def test_init_doc(self):
+        """A test of constructor doc"""
+        self.assertTrue(len(BaseModel.__init__.__doc__) > 10)
+
+    def test_json_string_doc(self):
+        """A test of to_json_string function doc"""
+        self.assertTrue(
+            len(Base.to_json_string.__doc__) > 10
+        )
+
+    def test_save_to_file_doc(self):
+        """A test of save_to_file function doc"""
+        self.assertTrue(
+            len(Base.save_to_file.__doc__) > 10
+        )
+
+    def test_from_json_string_doc(self):
+        """A test of from_json_string function doc"""
+        self.assertTrue(
+            len(Base.from_json_string.__doc__) > 10
+        )
+
+    def test_base_id0(self):
+        """Test the correct id of instance"""
+        o = Base()
+        self.assertEqual(o.id, 1)
+
+    def test_base_id1(self):
+        """Test the correct id of instance"""
+        o = Base()
+        self.assertEqual(o.id, 2)
+
+    def test_base_id2(self):
+        """Test the correct id of instance"""
+        o = Base()
+        self.assertEqual(o.id, 3)
+
+    def test_base_id3(self):
+        """Test the correct id of instance"""
+        o = Base(12)
+        self.assertEqual(o.id, 12)
+
+    def test_base_id4(self):
+        """Test the correct id of instance"""
+        o = Base()
+        self.assertEqual(o.id, 4)
+
+    def test_base_id5(self):
+        """Test the correct id of instance"""
+        o = Base(89)
+        self.assertEqual(o.id, 89)
+
+    def test_json_string0(self):
+        """Test if exist the to_json_strin function"""
+        self.assertTrue(Base.to_json_string(None))
+
+    def test_json_string1(self):
+        """Test if exist the to_json_string function"""
+        self.assertTrue(Base.to_json_string([]))
+
+    def test_json_string2(self):
+        """Test if exist the to_json_string function"""
+        self.assertTrue(
+            Base.to_json_string([{'id': 12}])
+        )
+
+    def test_json_string3(self):
+        """Test if the to_json_string function
+        retuns an string
+        """
+        self.assertIsInstance(
+            Base.to_json_string([{'id': 12}]),
+            str
+        )
+
+    def test_json_from_json0(self):
+        """Test if the from_json_strin function
+        retuns a empty list
+        """
+        self.assertEqual(
+            Base.from_json_string(None),
+            []
+        )
+
+    def test_json_from_json1(self):
+        """Test if the from_json_strin function
+        retuns a empty list
+        """
+        self.assertEqual(
+            Base.from_json_string("[]"),
+            []
+        )
+
+    def test_json_from_json2(self):
+        """Test if the from_json_strin exist
+        with an specific parameter
+        """
+        self.assertTrue(
+            Base.from_json_string('[{"id": 89}]')
+        )
+
+    def test_json_from_json3(self):
+        """Test if the from_json_strin exist
+        with an specific parameter
+        """
+        self.assertIsInstance(
+            Base.from_json_string('[{"id": 89}]'),
+            list
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
