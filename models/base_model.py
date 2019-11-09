868cc6ca9b9b21f29f99ba22dca3c56c3c422f76
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-09 17:25:07 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,6 @@ This module contains the BaseModel class
 """
 import datetime as d
 import uuid
-import models.engine.file_storage as FileStorage
 
 
 class BaseModel(object):
@@ -17,19 +16,17 @@ class BaseModel(object):
     """
     def __init__(self, *args, **kwargs):
         """Returns a BaseModel object"""
-        self.storage = FileStorage()
-        self.storage.reload()
         if kwargs is not None and len(kwargs) > 0:
             for i, arg in kwargs.items():
                 if i == "__class__":
                     arg = self.__class__
                 if i == "created_at":
                     arg = d.datetime.strptime(
-                        arg, "%Y-%m-%d %H:%M:%S.%f"
+                        arg, "%Y-%m-%dT%H:%M:%S.%f"
                     )
                 if i == "updated_at":
                     arg = d.datetime.strptime(
-                        arg, "%Y-%m-%d %H:%M:%S.%f"
+                        arg, "%Y-%m-%dT%H:%M:%S.%f"
                     )
                 setattr(self, i, arg)
         else:
@@ -50,22 +47,21 @@ class BaseModel(object):
 
     def save(self):
         """Method to updates the instance"""
-        import models.engine.file_storage as storage
         self.updated_at = d.datetime.now()
 
     def to_dict(self):
         """Returns a dictionary representation of this object"""
         dicver = dict()
         dicver.__setitem__("my_number", self.my_number)
-        dicver.__setitem__("my_name", self.name)
+        dicver.__setitem__("name", self.name)
         dicver.__setitem__("__class__", self.__class__.__name__)
         dicver.__setitem__(
             "updated_at",
-            self.updated_at.isoformat(' ')
+            self.updated_at.isoformat()
         )
         dicver.__setitem__("id", self.id)
         dicver.__setitem__(
             "created_at",
-            self.created_at.isoformat(' ')
+            self.created_at.isoformat()
         )
         return (dicver)
