1b49fcad3e3819c06b1530f0e23c589f40751fc5
DaniVasq <danivasq50@gmail.com>
2019-11-10 02:40:56 +0000

    [feature] adding create cmd to console.

--- console.py
+++ console.py
@@ -23,6 +23,19 @@ class HBNBCommand(cmd.Cmd):
         """An empty line and ENTER should not execute anything"""
         pass
 
+    def do_create(self, args):
+        """Creates new instance of BaseModel and prints id"""
+        v1 = args.split()
+
+        if len(v1) == 0:
+            print("** class name missing **")
+        else:
+            if v1[0] not in valid_classes:
+                print("** class doesn't exist **")
+            else:
+                obj = eval(v1[0])()
+                obj.save()
+                print(obj.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
