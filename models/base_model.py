a4807ebf6a6cf8d2ae9727042051874114c76cdd
DaniVasq <danivasq50@gmail.com>
2019-11-10 22:16:19 +0000

    [fixing basemode]

--- models/base_model.py
+++ models/base_model.py
@@ -1,74 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """Module BaseModel
-
 This module contains the BaseModel class
-
 """
-import copy
-import datetime as d
-from models import storage
+import models
+from datetime import datetime
 import uuid
-
-
-class BaseModel(object):
+class BaseModel():
     """BaseModel class
-
     A simple empty BaseModel class
     """
     def __init__(self, *args, **kwargs):
-        """Returns a BaseModel object"""
-        if kwargs is not None and len(kwargs) > 0:
+        if kwargs is not None and len(kwargs) != 0:
             for i, arg in kwargs.items():
                 if i == "__class__":
                     arg = self.__class__
                 if i == "created_at":
-                    arg = d.datetime.strptime(
+                    arg = datetime.strptime(
                         arg, "%Y-%m-%dT%H:%M:%S.%f"
                     )
                 if i == "updated_at":
-                    arg = d.datetime.strptime(
+                    arg = datetime.strptime(
                         arg, "%Y-%m-%dT%H:%M:%S.%f"
                     )
+                if i == "my_number":
+                    self.my_number = arg
+                if i == "name":
+                    self.name = arg
+                if i == "id":
+                    self.id = arg
                 setattr(self, i, arg)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = d.datetime.now()
-            self.updated_at = d.datetime.now()
-            storage.new(self)
-
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
     def __str__(self):
-        """Returns this object in a string format"""
-        return (
-            "[{:s}] ({:s}) {:s}"
-            .format(
-                str(self.__class__.__name__),
-                str(self.id),
-                str(self.__dict__)
-            )
-        )
-
+        """Oberwrite the __str__ magic method"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                            self.__dict__))
     def save(self):
         """Method to updates the instance"""
-        self.updated_at = d.datetime.now()
-        storage.save()
-
+        self.updated_at = datetime.now()
+        models.storage.save()
     def to_dict(self):
         """Returns a dictionary representation of this object"""
-        """
-        dicver = dict()
-        dicver.__setitem__("my_number", self.my_number)
-        dicver.__setitem__("name", self.name)
-        dicver.__setitem__("__class__", self.__class__.__name__)
-        dicver.__setitem__(
-            "updated_at",
-            self.updated_at.isoformat()
-        )
-        dicver.__setitem__("id", self.id)
-        dicver.__setitem__(
-            "created_at",
-            self.created_at.isoformat()
-        )
-        """
-        # return (copy.deepcopy(self.__dict__))
-        return (copy.deepcopy(self.__dict__))
+        dicver = self.__dict__.copy()
+        dicver["__class__"] = self.__class__.__name__
+        dicver["updated_at"] = self.__dict__["updated_at"].isoformat()
+        dicver["created_at"] = self.__dict__["created_at"].isoformat()
+        return (dicver)
