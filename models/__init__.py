c4d0839e4d1c9076833933c63ad2957935151003
DaniVasq <danivasq50@gmail.com>
2019-11-12 20:46:34 +0000

    [FIX] adding init file change

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A init configuration to the file_storage"""
+"""A init configuration to the file_storage."""
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
